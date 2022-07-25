650aa0df72ade8aff60795046725b1915b1d72bf
Ciber <cibermonster@gmail.com>
2022-07-25 13:12:28 -0400

    Starting button code

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const numberbtn = document.querySelector("button");
+const s2 = document.getElementById("s2");
+
+function number(x) {
+    s2.innerHTML += x;
+    console.log("Test");
+}
+
+
+numberbtn.addEventListener('click', myFunction());
+
